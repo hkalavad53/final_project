39c9d3c141c8394797573ce3b452e20dba084aaf
hkalavad <hkalavad@cougarnet.uh.edu>
2024-11-08 22:00:54 -0600

    updated_gfa

--- hifiasm/gfa_to_fa.sh
+++ hifiasm/gfa_to_fa.sh
@@ -2,7 +2,7 @@
 #SBATCH -J gfa_to_fa
 #SBATCH -o gfa_to_fa.%j
 #SBATCH -t 144:00:00
-#SBATCH -N 1 -n 10
+#SBATCH -N 1 -n 1
 #SBATCH --mem=120G
 #SBATCH --mail-user=hskalavad@gmail.com
 #SBATCH --mail-type=ALL
