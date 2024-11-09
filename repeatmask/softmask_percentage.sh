ba7498b7e7df6955401f11381de219be1f7815fd
hkalavad <hkalavad@cougarnet.uh.edu>
2024-11-09 11:08:25 -0600

    sm

---
+++ repeatmask/softmask_percentage.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH -J softmask_percentage
+#SBATCH -o softmask_percentage.%j
+#SBATCH -t 144:00:00
+#SBATCH -N 1 -n 1
+#SBATCH --mem=25G
+#SBATCH --mail-user=hskalavad@gmail.com
+#SBATCH --mail-type=ALL
+
+python /project/daane/hussain/Python/softmask_percentage.py /project/daane/hussain/final_project/raw_data/obeta_ncbi.fa
+
+python /project/daane/hussain/Python/softmask_percentage.py /project/daane/hussain/final_project/raw_data/obeta_ragtag_tama.fa
+
+python /project/daane/hussain/Python/softmask_percentage.py /project/daane/hussain/final_project/raw_data/obeta_ragtag_ncbi.fa
+
